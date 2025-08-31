e4387cec7561398217e171d69acabe30df9976bd
load28 <tjalsdud89@naver.com>
2025-08-31 20:12:22 +0900

    feat: add all atom hook

--- packages/react/src/hooks.ts
+++ packages/react/src/hooks.ts
@@ -1,5 +1,22 @@
 import { useCallback, useContext, useEffect, useState } from "react";
 import { createStoreContext } from "./context";
+import type { ContextQueryStore } from "@context-query/core";
+
+export function createUseStore<TAtoms extends Record<string, any>>(
+  StoreContext: ReturnType<typeof createStoreContext<TAtoms>>
+): () => ContextQueryStore<TAtoms> {
+  return () => {
+    const store = useContext(StoreContext);
+
+    if (!store) {
+      throw new Error(
+        "useStore must be used within a ContextQueryProvider"
+      );
+    }
+
+    return store;
+  };
+}
 
 export function createUseContextAtom<TAtoms extends Record<string, any>>(
   StoreContext: ReturnType<typeof createStoreContext<TAtoms>>
@@ -18,7 +35,9 @@ export function createUseContextAtom<TAtoms extends Record<string, any>>(
 
   return <TKey extends keyof TAtoms>(key: TKey) => {
     const store = useStore();
-    const [value, setValue] = useState<TAtoms[TKey]>(() => store.getAtomValue(key));
+    const [value, setValue] = useState<TAtoms[TKey]>(() =>
+      store.getAtomValue(key)
+    );
 
     useEffect(() => {
       const handleChange = (_: TKey, newValue: TAtoms[TKey]) => {
@@ -32,14 +51,18 @@ export function createUseContextAtom<TAtoms extends Record<string, any>>(
       };
     }, [key, store]);
 
-    const setAtom = useCallback((newValue: TAtoms[TKey] | ((prev: TAtoms[TKey]) => TAtoms[TKey])) => {
-      const currentValue = store.getAtomValue(key);
-      const updatedValue = typeof newValue === "function" 
-        ? (newValue as Function)(currentValue) 
-        : newValue;
-      
-      store.setAtomValue(key, updatedValue);
-    }, [key, store]);
+    const setAtom = useCallback(
+      (newValue: TAtoms[TKey] | ((prev: TAtoms[TKey]) => TAtoms[TKey])) => {
+        const currentValue = store.getAtomValue(key);
+        const updatedValue =
+          typeof newValue === "function"
+            ? (newValue as Function)(currentValue)
+            : newValue;
+
+        store.setAtomValue(key, updatedValue);
+      },
+      [key, store]
+    );
 
     return [value, setAtom] as const;
   };
@@ -62,7 +85,9 @@ export function createUseContextAtomValue<TAtoms extends Record<string, any>>(
 
   return <TKey extends keyof TAtoms>(key: TKey): TAtoms[TKey] => {
     const store = useStore();
-    const [value, setValue] = useState<TAtoms[TKey]>(() => store.getAtomValue(key));
+    const [value, setValue] = useState<TAtoms[TKey]>(() =>
+      store.getAtomValue(key)
+    );
 
     useEffect(() => {
       const handleChange = (_: TKey, newValue: TAtoms[TKey]) => {
@@ -97,16 +122,140 @@ export function createUseContextSetAtom<TAtoms extends Record<string, any>>(
 
   return <TKey extends keyof TAtoms>(
     key: TKey
-  ): ((value: TAtoms[TKey] | ((prev: TAtoms[TKey]) => TAtoms[TKey])) => void) => {
+  ): ((
+    value: TAtoms[TKey] | ((prev: TAtoms[TKey]) => TAtoms[TKey])
+  ) => void) => {
     const store = useStore();
 
-    return useCallback((newValue: TAtoms[TKey] | ((prev: TAtoms[TKey]) => TAtoms[TKey])) => {
-      const currentValue = store.getAtomValue(key);
-      const updatedValue = typeof newValue === "function" 
-        ? (newValue as Function)(currentValue) 
-        : newValue;
+    return useCallback(
+      (newValue: TAtoms[TKey] | ((prev: TAtoms[TKey]) => TAtoms[TKey])) => {
+        const currentValue = store.getAtomValue(key);
+        const updatedValue =
+          typeof newValue === "function"
+            ? (newValue as Function)(currentValue)
+            : newValue;
+
+        store.setAtomValue(key, updatedValue);
+      },
+      [key, store]
+    );
+  };
+}
+
+export function createUseAllAtoms<TAtoms extends Record<string, any>>(
+  StoreContext: ReturnType<typeof createStoreContext<TAtoms>>
+) {
+  const useStore = () => {
+    const store = useContext(StoreContext);
+
+    if (!store) {
+      throw new Error(
+        "useAllAtoms must be used within a ContextQueryProvider"
+      );
+    }
+
+    return store;
+  };
+
+  return (): [TAtoms, (newAtoms: Partial<TAtoms>) => void] => {
+    const store = useStore();
+    const [allValues, setAllValues] = useState<TAtoms>(() =>
+      store.getAllAtomValues()
+    );
+
+    useEffect(() => {
+      // Subscribe to all atoms by getting all keys and subscribing to each
+      const subscriptions: Array<{ unsubscribe: () => void }> = [];
+      const currentAtoms = store.getAllAtomValues();
       
-      store.setAtomValue(key, updatedValue);
-    }, [key, store]);
+      Object.keys(currentAtoms).forEach((key) => {
+        const subscription = store.subscribeToAtom(key as keyof TAtoms, () => {
+          setAllValues(store.getAllAtomValues());
+        });
+        subscriptions.push(subscription);
+      });
+
+      return () => {
+        subscriptions.forEach(sub => sub.unsubscribe());
+      };
+    }, [store]);
+
+    const updateAllAtoms = useCallback(
+      (newAtoms: Partial<TAtoms>) => {
+        store.updateAllAtoms(newAtoms);
+      },
+      [store]
+    );
+
+    return [allValues, updateAllAtoms] as const;
+  };
+}
+
+export function createUseAllAtomsValue<TAtoms extends Record<string, any>>(
+  StoreContext: ReturnType<typeof createStoreContext<TAtoms>>
+) {
+  const useStore = () => {
+    const store = useContext(StoreContext);
+
+    if (!store) {
+      throw new Error(
+        "useAllAtomsValue must be used within a ContextQueryProvider"
+      );
+    }
+
+    return store;
+  };
+
+  return (): TAtoms => {
+    const store = useStore();
+    const [allValues, setAllValues] = useState<TAtoms>(() =>
+      store.getAllAtomValues()
+    );
+
+    useEffect(() => {
+      // Subscribe to all atoms by getting all keys and subscribing to each
+      const subscriptions: Array<{ unsubscribe: () => void }> = [];
+      const currentAtoms = store.getAllAtomValues();
+      
+      Object.keys(currentAtoms).forEach((key) => {
+        const subscription = store.subscribeToAtom(key as keyof TAtoms, () => {
+          setAllValues(store.getAllAtomValues());
+        });
+        subscriptions.push(subscription);
+      });
+
+      return () => {
+        subscriptions.forEach(sub => sub.unsubscribe());
+      };
+    }, [store]);
+
+    return allValues;
+  };
+}
+
+export function createUseUpdateAllAtoms<TAtoms extends Record<string, any>>(
+  StoreContext: ReturnType<typeof createStoreContext<TAtoms>>
+) {
+  const useStore = () => {
+    const store = useContext(StoreContext);
+
+    if (!store) {
+      throw new Error(
+        "useUpdateAllAtoms must be used within a ContextQueryProvider"
+      );
+    }
+
+    return store;
+  };
+
+  return (): ((newAtoms: Partial<TAtoms>) => void) => {
+    const store = useStore();
+
+    return useCallback(
+      (newAtoms: Partial<TAtoms>) => {
+        store.updateAllAtoms(newAtoms);
+      },
+      [store]
+    );
   };
 }
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,7 +1,11 @@
 import { 
   createUseContextAtom, 
   createUseContextAtomValue, 
-  createUseContextSetAtom 
+  createUseContextSetAtom,
+  createUseStore,
+  createUseAllAtoms,
+  createUseAllAtomsValue,
+  createUseUpdateAllAtoms
 } from "./hooks";
 import { createReactContextQuery } from "./provider";
 
@@ -12,11 +16,19 @@ export function createContextQuery<TAtoms extends Record<string, any>>() {
   const useContextAtom = createUseContextAtom<TAtoms>(StoreContext);
   const useContextAtomValue = createUseContextAtomValue<TAtoms>(StoreContext);
   const useContextSetAtom = createUseContextSetAtom<TAtoms>(StoreContext);
+  const useStore = createUseStore<TAtoms>(StoreContext);
+  const useAllAtoms = createUseAllAtoms<TAtoms>(StoreContext);
+  const useAllAtomsValue = createUseAllAtomsValue<TAtoms>(StoreContext);
+  const useUpdateAllAtoms = createUseUpdateAllAtoms<TAtoms>(StoreContext);
   
   return {
     ContextQueryProvider,
     useContextAtom,
     useContextAtomValue,
     useContextSetAtom,
+    useStore,
+    useAllAtoms,
+    useAllAtomsValue,
+    useUpdateAllAtoms,
   };
 }
